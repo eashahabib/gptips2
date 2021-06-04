0529a779b86557cd4caf77237e3c2d5ac55e2bcb
eashahabib <52130107+eashahabib@users.noreply.github.com>
2021-06-04 09:10:18 +0500

    upddate

--- easha_tutorial.m
+++ easha_tutorial.m
@@ -4,40 +4,56 @@ clc;
 % 
 % %gp = rungp(@spatial_evol_config);
 %figure; 
-for i=1:4
+for i=1:5
     i
     
-    gp = rungp(@gpTEST1_config);
-    eval(['GPTIPS1_gp_multiGD' num2str(i) '= gp;']);
-    gppretty(gp, 'best')
+%     gp = rungp(@gpTEST1_config);
+%     eval(['GPTIPS1_gp' num2str(i) '= gp;']);
+%     gppretty(gp, 'best')
+%     
     
-    gp = rungp(@gpTEST2_1_config);
-    eval(['JINAY1_gp_multiGD' num2str(i) '= gp;']);
-    gppretty(gp, 'best')
-    
-    gp = rungp(@gpTEST2_2_config);
-    eval(['JINAY2_gp_multiGD' num2str(i) '= gp;']);
-    gppretty(gp, 'best')
-    
+%     gp = rungp(@gpTEST2_1_config);
+%     eval(['JINAY1_gp_newton' num2str(i) '= gp;']);
+%     gppretty(gp, 'best')
+%     
+%     gp = rungp(@gpTEST2_2_config);
+%     eval(['JINAY2_gp_newton' num2str(i) '= gp;']);
+%     gppretty(gp, 'best')
+%     
     gp = rungp(@gpTEST3_1_config);
-    eval(['ARMANI1_gp_multiGD' num2str(i) '= gp;']);
+    eval(['ARMANI1_gp_newton' num2str(i) '= gp;']);
     gppretty(gp, 'best')
+    
+    figure(1); 
+plot(1:gp.state.count, gp.results.history.bestfitness); hold on;
     
     gp = rungp(@gpTEST3_2_config);
-    eval(['ARMANI2_gp_multiGD' num2str(i) '= gp;']);
+    eval(['ARMANI2_gp_newton' num2str(i) '= gp;']);
     gppretty(gp, 'best')
+    
+    figure(2); 
+plot(1:gp.state.count, gp.results.history.bestfitness); hold on;
     
     gp = rungp(@gpTEST3_3_config);
-    eval(['ARMANI3_gp_multiGD' num2str(i) '= gp;']);
+    eval(['ARMANI3_gp_newton' num2str(i) '= gp;']);
     gppretty(gp, 'best')
+    
+    figure(3); 
+plot(1:gp.state.count, gp.results.history.bestfitness); hold on;
     
     gp = rungp(@gpTEST3_4_config);
-    eval(['ARMANI4_gp_multiGD' num2str(i) '= gp;']);
+    eval(['ARMANI4_gp_newton' num2str(i) '= gp;']);
     gppretty(gp, 'best')
     
+    figure(4); 
+plot(1:gp.state.count, gp.results.history.bestfitness); hold on;
+    
     gp = rungp(@gpTEST3_5_config);
-    eval(['ARMANI5_gp_multiGD' num2str(i) '= gp;']);
+    eval(['ARMANI5_gp_newton' num2str(i) '= gp;']);
     gppretty(gp, 'best')
+    
+    figure(5); 
+plot(1:gp.state.count, gp.results.history.bestfitness); hold on;
 
     
 %     A = gp.results.history;
@@ -52,9 +68,12 @@ for i=1:4
 %     
 %     myStats_pre.FITiters(i) = length(A.bestfitness);
 %     myStats_pre.FITtime(i) = B.runTimeElapsed;
+
+figure(1); 
+plot(1:gp.state.count, gp.results.history.bestfitness); hold on;
 end
 
-save('filewithresultsCORRECTIONMAC_allsets_12iters');
+save('filewithresultsTEST');
 
 % grid minor;
 % legend;
